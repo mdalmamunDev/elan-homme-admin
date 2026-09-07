0ef24e9c02254fb75e5a961a6c4d31df6349f10f
Mamun <mamun.dev.pro@gmail.com>
2026-09-07 12:30:09 +0600

    up

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: ["elan-homme-admin.vercel.app"], // Only allow this host
+    host: "0.0.0.0", // Listen on all interfaces, making it accessible externally
+    port: 5173, // Set your desired port
+    strictPort: true, // Ensure the port is strictly bound
+    cors: true, // Enable CORS if necessary for external access
+  },
 })
